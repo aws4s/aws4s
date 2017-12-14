bf3d032a9dcbdf1db57901868b20a2d2c4ab9619
Amr Hassan <amr.hassan@gmail.com>
2017-12-14 11:57:14 +0100

    Factoring out boilerplate

--- src/main/scala/org/aws4s/kms/GrantTokensParam.scala
+++ src/main/scala/org/aws4s/kms/GrantTokensParam.scala
@@ -2,10 +2,9 @@ package org.aws4s.kms
 
 private [kms] case class GrantTokensParam(value: GrantTokens) extends KmsParam[GrantTokens](
   "GrantTokens",
-  gts => if (gts.tokens.length > 10)
-    Some("token count not in [0,10]")
-  else if (gts.tokens.exists(_.length > 8192))
-    Some("one token length cannot exceed 8192")
-  else
-    None
+  _.tokens match {
+    case tokens if tokens.length > 10 => Some("token count not in [0,10]")
+    case tokens if tokens.exists(_.length > 8192) => Some("one token length cannot exceed 8192")
+    case _ => None
+  }
 )
