7b984a4b489ad26b712deac254a56cea08f08a22
Amr Hassan <amr.hassan@gmail.com>
2018-02-11 21:14:29 +0100

    SQS with core

--- src/main/scala/org/aws4s/sqs/SqsParam.scala
+++ src/main/scala/org/aws4s/sqs/SqsParam.scala
@@ -1,12 +1,11 @@
 package org.aws4s.sqs
 
 import cats.Show
-import cats.implicits._
-import org.aws4s.core.{Param2, PrimitiveParam}
+import org.aws4s.core.{Param2, ParamRenderer, PrimitiveParam}
 
 private[sqs] abstract class SqsParam[A: Show](
     val name:      String,
     val validator: Param2.Validator[A]
 ) extends PrimitiveParam[A, String] {
-  override private[aws4s] final val renderer: Param2.Renderer[A, String] = _.show
+  override private[aws4s] final val renderer: Param2.Renderer[A, String] = ParamRenderer.show
 }
