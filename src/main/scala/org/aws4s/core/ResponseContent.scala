4baad9e4f68488c4f8bd072efd1a5d0f539a840a
Amr Hassan <amr.hassan@gmail.com>
2018-02-18 19:45:26 +0100

    Correct decoding of DynamoDB error messages

--- src/main/scala/org/aws4s/core/ResponseContent.scala
+++ src/main/scala/org/aws4s/core/ResponseContent.scala
@@ -1,10 +1,11 @@
 package org.aws4s.core
 
-import cats.effect.Effect
+import cats.data.NonEmptyList
+import cats.effect.{Effect, Sync}
 import io.circe.Json
-import org.http4s.EntityDecoder
+import org.http4s.{EntityDecoder, MediaRange, Message}
 import org.http4s.scalaxml._
-import ExtraEntityDecoderInstances._
+import org.http4s.circe._
 
 private[aws4s] sealed trait ResponseContent {
   final def tryParse[A](pf: PartialFunction[ResponseContent, Option[A]]): Option[A] =
@@ -21,6 +22,16 @@ private[aws4s] object ResponseContent {
   implicit def entityDecoder[F[_]: Effect]: EntityDecoder[F, ResponseContent] =
     EntityDecoder[F, scala.xml.Elem].map(elem => XmlContent(elem)).widen[ResponseContent] orElse
       EntityDecoder[F, Json].map(json         => JsonContent(json)).widen[ResponseContent] orElse
+      inclusiveJsonEntityDecoder.map(json     => JsonContent(json)).widen[ResponseContent] orElse
       EntityDecoder[F, String].map(text       => StringContent(text)).widen[ResponseContent] orElse
       EntityDecoder[F, Unit].map(_            => NoContent).widen[ResponseContent]
+
+  private def inclusiveJsonEntityDecoder[F[_]: Sync]: EntityDecoder[F, Json] = {
+    val json = jsonDecoder[F]
+    val extraMediaRanges = NonEmptyList.of(
+      "application/x-amz-json-1.0"
+    ) map (mr => MediaRange.parse(mr).getOrElse(throw new RuntimeException(s"Invalid Media Range: $mr")))
+    val allMediaRanges = extraMediaRanges concat json.consumes.toList
+    EntityDecoder.decodeBy[F, Json](allMediaRanges.head, allMediaRanges.tail: _*)((msg: Message[F]) => json.decode(msg, strict = false))
+  }
 }
